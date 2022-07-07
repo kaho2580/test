fbbd10cbd7837d718727ac2f4b7236048290f898
EC2 Default User <ec2-user@ip-172-31-39-244.ec2.internal>
2022-07-08 05:06:07 +0900

    07/08

--- blog/app/Post.php
+++ blog/app/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+ /**
+ * 特定IDのpostを表示する
+ *
+ * @params Object Post // 引数の$postはid=1のPostインスタンス
+ * @return Reposnse post view
+ */
+public function getPaginateByLimit(int $limit_count = 11)
+{
+    return $this->orderBy('created_at', 'DESC')->paginate($limit_count);
+}
+
 }
