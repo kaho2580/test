12d73b90b5445225cf4ac10a686cff4ede445e9e
EC2 Default User <ec2-user@ip-172-31-39-244.ec2.internal>
2022-07-09 00:43:03 +0900

    8-4

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -15,4 +15,4 @@ Route::get('/posts', 'PostController@index');
 Route::get('/','PostController@index');
 Route::get('/posts/create','PostController@create');
 Route::get('/posts/{post}','PostController@show');
-
+Route::post('/posts', 'PostController@store');
